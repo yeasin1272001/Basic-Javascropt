fd2b4031b6e48c3d644f221b09f16033754b77c5
unknown <yeasinmiah1272001@gmail.com>
2026-02-13 15:19:29 +0600

    if-else

--- conditional.js
+++ conditional.js
@@ -2,3 +2,100 @@
 // console.log(5<10);
 // console.log(5==10);
 // console.log(10==10);
+
+
+
+// var appalePrice = 250;
+
+// if(appalePrice <300){
+//     console.log("Give me some apple");
+// }
+
+// var biriyaniPrice = 500;
+
+
+// var weight = 40;
+// if(weight < 20){
+//     console.log("I will cary it");
+// }
+
+// else{
+//     console.log("I will rent a rickhsaw");
+// }
+
+
+// const salary = 25000;
+
+// isBCS = true;
+
+
+// if(salary > 20000){
+//     console.log("Good Boys");
+// }
+
+// else{
+//     console.log("Bad boys");
+// }
+
+
+
+// const salary = 25000;
+
+// isBCS = true;
+
+
+// if(salary < 20000){
+//     console.log("Good Boys");
+// }
+
+// else{
+//     console.log("Bad boys");
+// }
+
+
+// const salary = 25000;
+// const height = 61
+
+// isBCS = true;
+
+
+// if(salary > 20000 && height > 66){
+//     console.log("Good Boys");
+// }
+
+// else{
+//     console.log("Bad boys");
+// }
+
+
+// const salary = 25000;
+// const height = 61
+
+// isBCS = true;
+
+
+// if(salary > 20000 && height > 55 ){
+//     console.log("Good Boys");
+// }
+
+// else{
+//     console.log("Bad boys");
+// }
+
+
+// const salary = 25000;
+// const height = 61
+
+// isBCS = true;
+
+
+// if(salary > 20000 || height > 66 ){
+//     console.log("Good Boys");
+// }
+
+// else{
+//     console.log("Bad boys");
+// }
+
+
+
