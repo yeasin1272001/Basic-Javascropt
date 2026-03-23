fab5fb78dddc9dde5c612a87e44ed148f0d58062
unknown <yeasinmiah1272001@gmail.com>
2026-03-23 10:28:22 +0600

    comparism

---
+++ comparism.js
@@ -0,0 +1,53 @@
+// console.log(5>10)
+// console.log(5<10)
+// console.log(10==10)
+// console.log(5<=10)
+
+// var mangoPrice = 50;
+// var guava  = 500;
+
+// console.log(mangoPrice > guava)
+// console.log(mangoPrice < guava)
+
+// if(mangoPrice > guava){
+//     console.log("Thik ase");
+// }
+// else{
+//     console.log("Vule hoyse")
+// }
+
+// Even or odd
+
+// const number= 7;
+
+// if(number % 2 == 0){
+//     console.log("Even number");
+// }
+// else{
+//     console.log("odd number")
+// }
+
+// const age = 20;
+// if(age > 18){
+//     console.log("apni melai aste parben");
+// }
+// else{
+//     console.log("apanar boyes hoyni.aste parben na");
+// }
+
+// const num1 = 55;
+// const num2 = 50;
+// const num3 = 40;
+// if(num1 > num2){
+//     console.log("Number 1 is big");
+// }
+// else if (num2 > num3) {
+//     console.log(" Number 2 is big")
+    
+// } else {
+//     console.log("Number 3 is big")
+    
+// }
+
+
+
---
+++ condition.js
@@ -0,0 +1,80 @@
+// Problem-1: check even or odd number--------------
+// const num1 = 7;
+// if(num1 % 2 ==0){
+//     console.log("this is positive")
+// }
+// else{
+//     console.log("others number")
+// }
+
+// Problem-2:Check user----------------------------------
+
+// const password = "1234";
+// const userName = "Yeasin";
+// if(password === "1234" && userName === "Yeasin"){
+//     console.log("User Login Success")
+// }
+// else{
+//     console.log("Your user or password wrong")
+// }
+
+// Problem-3: check pass or fail--------------------
+
+// const marks = 35;
+// if(marks >= 40){
+//     console.log("pass")
+// }
+// else{
+//     console.log("fail")
+// }
+// -------------------------------------
+// const money = 200;
+// if(money > 300){
+//     console.log("amara cinema dekte jabo");
+// }
+// else{
+//     console.log("Taka kom pore jabo")
+// }
+
+// -------------------------------------------------
+// const salary = 22000;
+// const isEnneer = true;
+// const height = 60
+// if(salary > 18000 && height > 66){
+//     console.log("Ok tomi meye dekte aso");
+// }
+// else{
+//     console.log("Tomar salary kom meye dekar sojug nai tomar")
+
+// -------------------------------------
+// const salary = 22000;
+// const isEnneer = true;
+// const height = 60
+// if(salary > 18000 || height > 66){
+//     console.log("Ok tomi meye dekte aso");
+// }
+// else{
+//     console.log("Tomar salary kom meye dekar sojug nai tomar")
+
+// ---------------------------------------------------------
+// const salary = 22000;
+// const isEnneer = true;
+// const height = 60
+// if(salary > 18000 && height > 55 && isEnneer === true){
+//     console.log("Ok tomi meye dekte aso");
+// }
+// else{
+//     console.log("Tomar salary kom meye dekar sojug nai tomar")
+// }
+
+// -------------------------------------------------
+
+// const salary = 22000;
+// const isEnneer = true;
+// const height = 60
+// if(salary > 26000 || height > 66 || isEnneer == true){
+//     console.log("Ok tomi meye dekte aso");
+// }
+// else{
+//     console.log("Tomar salary kom meye dekar sojug nai tomar")
+// }
